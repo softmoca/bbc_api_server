85b88bb5c4ed11d0880e28ef136f53251522ffd7
softmoca <acyc0909@gmail.com>
2024-03-26 23:10:07 +0900

    feat : creat chats&& enter chats && sned_message

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -7,6 +7,7 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsException,
 } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
 import { CreateChatDto } from "./dto/create-chat.dto";
@@ -14,6 +15,11 @@ import { ChatsService } from "./chats.service";
 import { UserService } from "src/user/user.service";
 import { AuthService } from "src/auth/auth.service";
 import { ChatsMessagesService } from "./messages/messages.service";
+import { UsersModel } from "src/entites/user.entity";
+import { UseFilters, UsePipes, ValidationPipe } from "@nestjs/common";
+import { SocketCatchHttpExceptionFilter } from "src/common/exception-filter/socket-catch-http.exception-filter";
+import { EnterChatDto } from "./dto/enter-chat.dto";
+import { CreateMessagesDto } from "./messages/dto/create-messages.dto";
 
 @WebSocketGateway({
   namespace: "chats",
@@ -38,15 +44,121 @@ export class ChatsGateway
     console.log(`on disconnect called : ${socket.id}`);
   }
 
-  handleConnection(socket: Socket) {
+  async handleConnection(socket: Socket & { user: UsersModel }) {
     console.log(`on connect called : ${socket.id}`);
+
+    const headers = socket.handshake.headers;
+    const rawToken = headers["authorization"];
+
+    if (!rawToken) {
+      console.log("서버 : 토큰이 없는 에러로 인해 연결을 끊습니다.");
+      socket.emit("exception", {
+        data: "토큰이 없는 에러로 인해 연결을 끊습니다.",
+      });
+      socket.disconnect();
+    }
+
+    try {
+      const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+      const payload = this.authService.verifyToken(token);
+      const user = await this.usersService.getUserByEmail(payload.email);
+
+      socket.user = user;
+
+      return true;
+    } catch (e) {
+      console.log("서버 : 소켓 통신시 에러 발생으로 인해 연결을 끊습니다");
+      socket.emit("exception", {
+        data: "소켓 통신시 에러 발생으로 인해 연결을 끊습니다.",
+      });
+
+      socket.disconnect();
+    }
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
   @SubscribeMessage("create_chat")
   async createChat(
     @MessageBody() data: CreateChatDto,
-    @ConnectedSocket() socket: Socket
+    @ConnectedSocket() socket: Socket & { user: UsersModel }
   ) {
+    console.log(socket.user);
     const chat = await this.chatsService.createChat(data);
+  }
+
+  @SubscribeMessage("enter_chat")
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
+  async enterChat(
+    // 방의 chat ID들을 리스트로 받는다.
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
+    @MessageBody() data: EnterChatDto
+  ) {
+    for (const chatId of data.chatIds) {
+      const exists = await this.chatsService.checkIfChatExists(chatId);
+
+      if (!exists) {
+        throw new WsException({
+          code: 100,
+          message: `존재하지 않는 chat 입니다. chatId: ${chatId}`,
+        });
+      }
+    }
+
+    socket.join(data.chatIds.map((x) => x.toString()));
+  }
+
+  @SubscribeMessage("send_message")
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
+  async sendMessage(
+    @MessageBody() dto: CreateMessagesDto,
+    @ConnectedSocket() socket: Socket & { user: UsersModel }
+  ) {
+    const chatExists = await this.chatsService.checkIfChatExists(dto.chatId);
+
+    if (!chatExists) {
+      throw new WsException(
+        `존재하지 않는 채팅방입니다. Chat ID : ${dto.chatId}`
+      );
+    }
+
+    const message = await this.messagesService.createMessage(
+      dto,
+      socket.user.id
+    );
+
+    socket
+      .to(message.chat.id.toString())
+      .emit("receive_message", message.message);
   }
 }
